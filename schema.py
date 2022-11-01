0a6db26c0b8088e0834529771bdba5bbea7fd969
ebreitmo <elena@epcc.ed.ac.uk>
2022-11-01 13:44:48 +0000

    Create schema.py

---
+++ schema.py
@@ -0,0 +1,16 @@
+from voluptuous import Any
+from voluptuous import Optional
+from voluptuous import Schema
+
+
+schema = {
+    Optional("skip_repository_checks_reason"): Any(str, None),
+    Optional("skip_license_checks_reason"): Any(str, None),
+    Optional("skip_registry_checks_reason"): Any(str, None),
+    Optional("skip_citation_checks_reason"): Any(str, None),
+    # Optional("skip_checklist_checks_reason"): Any(str, None),
+    skip_checklist_checks_reason: "I'm using the Codacy dashboard to guide my development"
+    Optional("ignore_commented_badges"): Any(bool, None)
+}
+
+validate_against_schema = Schema(schema)
